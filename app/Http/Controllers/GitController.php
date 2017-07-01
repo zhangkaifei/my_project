b35f4078ec538ef71b19c71dcb9f85db02e871ff
zhangkaifei <175002316@qq.com>
2017-07-01 17:44:41 +0800

    对gitcontroller这个文件再次做了修改

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -11,6 +11,7 @@ class GitController extends Controller
     public function checkConflict()
     {
         echo '在test_confict分支下我又修改了这个文件';
+        echo '再次在test_conflict分支下修改了这个文件';
         return '测试文件冲突是怎么产生的';
     }
 }
